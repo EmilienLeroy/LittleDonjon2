1f930905edf5b5df9d745bd4f74aa065ced5bcf2
Emilien Leroy <emilien1005@hotmail.fr>
2024-04-08 22:40:19 +0200

    🚧 Add the attack animation

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,21 +2,25 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 const DEFAULT_ROTATION = -180;
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _unhandled_key_input(event):
+	if event.is_action_pressed('attack'):
+		attack();
+		
+	if event.is_action_pressed('block'):
+		block();
+		
+	if event.is_action_pressed('dash'):
+		dash();
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -58,3 +62,16 @@ func get_model_rotation(direction: Vector3) -> float:
 			return deg_to_rad(DEFAULT_ROTATION * 0.75);
 
 		return 0;
+
+
+func attack():
+	if ($AnimationTree.get('parameters/Trigger Attack/active')):
+		return;
+
+	$AnimationTree.set('parameters/Trigger Attack/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE);
+	
+func block():
+	pass;
+	
+func dash():
+	pass;
