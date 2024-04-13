f3381ae93f071de149c8c1d30217bd75c92b940e
Emilien Leroy <emilien1005@hotmail.fr>
2024-04-13 17:57:19 +0200

    🚧 Improving combo animation

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -9,7 +9,7 @@ var current_direction: Vector3 = Vector3.ZERO;
 var attack_combo = 0;
 
 func _ready():
-	$AnimationTree.connect('animation_finished', on_animation_finished);
+	$AttackTimer.connect('timeout', on_combo_timeout);
 
 func _unhandled_key_input(event):
 	if event.is_action_pressed('attack'):
@@ -29,9 +29,13 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	if attack_combo == 2 and get_animation_state('TriggerAttackFinal'):
-		walk_speed = SPEED / 1.5;
-
+	if attack_combo == 0 and get_animation_state('TriggerAttackFinal'):
+		if (get_animation_time('AttackFinal') > 0.5 and get_animation_time('AttackFinal') < 0.9):
+			walk_speed = SPEED / 1.7;
+			
+		if (get_animation_time('AttackFinal') > 0.9 and get_animation_time('AttackFinal') < 1.2):
+			walk_speed = SPEED * 1.5;
+	
 	if direction:
 		$Model.rotation.y = lerp_angle($Model.rotation.y, get_model_rotation(direction), 0.3);
 		transition_animation('Locomotion', 'walk');
@@ -74,23 +78,35 @@ func move_to(direction: Vector3, speed: float):
 
 func attack():
 	if (
-		get_animation_state('TriggerAttack') 
-		or get_animation_state('TriggerAttackReverse')
-		or get_animation_state('TriggerAttackFinal')
+		(get_animation_state('TriggerAttack') and get_animation_time('Attack') < 0.5) 
+		or (get_animation_state('TriggerAttackReverse') and get_animation_time('AttackReverse') < 1.2)
+		or (get_animation_state('TriggerAttackFinal') and get_animation_time('AttackFinal') < 1.2)
 	):
 		return;
 
+	fade_out_animation('TriggerAttackFinal')
+
 	if (attack_combo == 0):
+		$AttackTimer.start(0);
+		seek_animation('AttackTimeSeek', 0);
 		fire_animation('TriggerAttack');
+		attack_combo = attack_combo + 1;
+		
+		return;
 
 	if (attack_combo == 1):
 		seek_animation('AttackReverseTimeSeek', 0.5);
 		fire_animation('TriggerAttackReverse');
+		attack_combo = attack_combo + 1;
+		
+		return;
 		
 	if (attack_combo == 2):
+		$AttackTimer.stop();
 		seek_animation('AttackFinalTimeSeek', 0.3);
 		fire_animation('TriggerAttackFinal');
-		
+		attack_combo = 0;
+	
 
 func block():
 	pass;
@@ -100,33 +116,31 @@ func dash():
 	move_and_slide();
 	seek_animation('DashTimeSeek', 0.25);
 	fire_animation('TriggerDash');
-
-func on_animation_finished(animation: StringName):
-	if (animation == 'Attack' or animation == 'AttackReverse'):
-		attack_combo = attack_combo + 1
-		await get_tree().create_timer(0.5).timeout;
-		
-		if (
-			!get_animation_state('TriggerAttack')
-			and !get_animation_state('TriggerAttackReverse')
-			and !get_animation_state('TirggerAttackFinal')
-		):
-			attack_combo = 0;
-
-		return;
 	
-	if (animation == 'AttackSlash'):
-		attack_combo = 0;
-		return;
+
+func on_combo_timeout():
+	attack_combo = 0;
 
 func get_animation_state(animation: String):
 	return $AnimationTree.get('parameters/' +animation+ '/active');
 
 func fire_animation(animation: String):
 	$AnimationTree.set('parameters/'+ animation +'/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE);
+
+func abort_animation(animation: String):
+	$AnimationTree.set('parameters/'+ animation +'/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT);
 	
+func fade_out_animation(animation: String):
+	$AnimationTree.set('parameters/'+ animation +'/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FADE_OUT);
+
 func seek_animation(animation: String, value: float):
 	$AnimationTree.set('parameters/'+ animation +'/seek_request', value);
 
 func transition_animation(animation: String, state: String):
 	$AnimationTree.set('parameters/'+ animation +'/transition_request', state);
+
+func set_animation_time(animation: String, time: float):
+	$AnimationTree.set('parameters/'+ animation +'/time', time);
+
+func get_animation_time(animation: String):
+	return $AnimationTree.get('parameters/'+ animation +'/time');
