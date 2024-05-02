c677118aa1694a533ef655b7e415991e27df59f7
Emilien Leroy <emilien1005@hotmail.fr>
2024-05-02 23:35:53 +0200

    🚧 Add boss and enter boss music

--- scenes/EnterBoss/EnterBoss.gd
+++ scenes/EnterBoss/EnterBoss.gd
@@ -4,5 +4,7 @@ var boss = load("res://scenes/Boss.tscn");
 
 func _ready():
 	$AnimationPlayer.play('cinematic');
+	$AudioAnimation.play('cinematic')
+	$AudioStreamPlayer.play(21)
 	await $AnimationPlayer.animation_finished;
 	Transition.transition_to(boss);
